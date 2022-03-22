818d0151c24217de85eb2bbd2c69b23a1d2fd081
Andréas Idh <david.andreas.idh@sundsvall.se>
2022-03-22 12:47:00 +0100

    Switch

--- packages/switch/stories/switch.stories.tsx
+++ packages/switch/stories/switch.stories.tsx
@@ -2,20 +2,71 @@ import { FormControl, FormLabel } from "@sk-web-gui/forms";
 import { Switch } from "../src";
 
 export default {
-  title: "Design System/Komponenter/WIP/Toggel",
+  title: "Design System/Komponenter/Toggel/Komponent",
   component: Switch,
   parameters: {
     controls: { hideNoControlsWarning: true },
   },
 };
 
-export const Toggel = () => ( 
+export const Template = (args) => (
   <div className="space-x-2">
-    <Switch aria-label="example switch" />
-    <Switch aria-label="example switch 2" defaultChecked />
+    <Switch {...args} aria-label="example switch 1" />
+
+    <h6 className="mt-12">Form Control</h6>
+    <FormControl className="flex items-center">
+      <FormLabel htmlFor="are-you-sure" className="mb-0 mr-2">
+        Är du verkligen säker?
+      </FormLabel>
+      <Switch {...args} aria-label="example switch 2" id="are-you-sure" />
+    </FormControl>
   </div>
 );
 
+Template.storyName = 'Komponent';
+
+Template.argTypes = {
+  defaultChecked: {
+    type: { name: 'string', required: false },
+    description: 'Sets defaultChecked',
+    table: {
+      defaultValue: { summary: 'false' },
+    },
+    control: 'boolean',
+    defaultValue: false,
+  },
+  disabled: {
+    type: { name: 'string', required: false },
+    description: 'Sets disabled',
+    table: {
+      defaultValue: { summary: 'false' },
+    },
+    control: 'boolean',
+    defaultValue: false,
+  },
+  size: {
+    type: { name: 'string', required: false },
+    description: 'Sets size',
+    table: {
+      defaultValue: { summary: 'md' },
+    },
+    options: ['sm', 'md', 'lg'],
+    control: 'select',
+    defaultValue: 'md',
+  },
+  color: {
+    type: { name: 'string', required: false },
+    description: 'Sets color',
+    table: {
+      defaultValue: { summary: 'primary' },
+    },
+    options: ['primary', 'secondary'],
+    control: 'select',
+    defaultValue: 'primary',
+  },
+};
+
+/*
 export const disabled = () => (
   <div className="space-x-2">
     <Switch aria-label="example switch" disabled />
@@ -38,6 +89,7 @@ export const Storlekar = () => (
   </div>
 );
 
+
 export const formControl = () => (
   <FormControl className="flex items-center">
     <FormLabel htmlFor="email-alerts" className="mb-0 mr-2">
@@ -46,3 +98,4 @@ export const formControl = () => (
     <Switch aria-label="example switch" id="email-alerts" />
   </FormControl>
 );
+*/
