c4a352bb932750b02d886cabdb14968c4e731c3e
Andréas Idh <david.andreas.idh@sundsvall.se>
2022-03-23 13:23:37 +0100

    Input

--- packages/forms/stories/form-control.stories.tsx
+++ packages/forms/stories/form-control.stories.tsx
@@ -1,7 +1,7 @@
 import { FormControl, FormLabel, Input, FormHelperText, FormErrorMessage } from "../src";
 
 export default {
-  title: "Design System/Komponenter/WIP/Textfält",
+  title: "Design System/Komponenter/Textfält/Komponent",
   component: FormControl,
   parameters: {
     controls: { hideNoControlsWarning: true },
