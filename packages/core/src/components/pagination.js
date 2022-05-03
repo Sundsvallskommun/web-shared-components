26a92af11603fd8eb01fc58008731b3e5bfe1765
Tim Lind <tim.lind@devize.se>
2022-05-03 16:30:48 +0200

    adjustments to match figma design

--- packages/core/src/components/pagination.js
+++ packages/core/src/components/pagination.js
@@ -4,10 +4,6 @@ module.exports = Pagination = () => ({
 
     "&-sm": {
       "@apply text-xs": {},
-
-      ".pagination-pageLabel": {
-        "@apply px-xs py-xs": {},
-      },
     },
 
     "&-md": {
@@ -25,7 +21,7 @@ module.exports = Pagination = () => ({
     "&-pageLabel": {
       "@apply cursor-pointer m-sm": {},
       width: "1.5em",
-      height: "1.8em",
+      height: "1.75em",
 
       "&[aria-disabled='true']": {
         "@apply border-primary border-b cursor-default text-primary": {},
@@ -33,11 +29,11 @@ module.exports = Pagination = () => ({
     },
 
     "&-prevNextButton": {
-      "@apply inline-flex items-center  sm:w-auto inline-flex flex-row leading-none":
+      "@apply ml-4 inline-flex items-center sm:w-auto inline-flex flex-row leading-none":
         {},
 
       "&[data-reverse=true]": {
-        "@apply flex-row-reverse": {},
+        "@apply mr-4 ml-0 flex-row-reverse": {},
 
         ".pagination-prevNextButton-label": {
           "@apply mr-sm ml-0": {},
@@ -59,10 +55,10 @@ module.exports = Pagination = () => ({
       },
 
       "&-icon": {
-        "@apply align-text-bottom leading-none flex items-center": {},
+        "@apply m-1 align-text-bottom leading-none flex items-center": {},
 
         ".material-icons-outlined": {
-          fontSize: "1em",
+          fontSize: ".675em",
         },
       },
     },
