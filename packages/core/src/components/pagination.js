4602af70c8f0790ce6b91c4d1cc717439ada0621
Henrik Sandström <99892539+hensankom@users.noreply.github.com>
2022-05-04 09:53:33 +0200

    Feature/pagination komponent (#6)
    * initial commit
    
    * baseline alignment and changelog fix
    
    * Some changes to handle page changes. Specifically, changing the active page from the parnet should not trigger the same logic as changing page by clicking inside the pagination.
    
    * Styling
    
    * adjustments to match figma design
    
    Co-authored-by: Tim Lind <tim.lind@devize.se>

---
+++ packages/core/src/components/pagination.js
@@ -0,0 +1,70 @@
+module.exports = Pagination = () => ({
+  ".pagination": {
+    "@apply select-none": {},
+
+    "&-sm": {
+      "@apply text-xs": {},
+    },
+
+    "&-md": {
+      "@apply text-lg": {},
+    },
+
+    "&-lg": {
+      "@apply text-xl": {},
+    },
+
+    "&-list": {
+      "@apply flex items-baseline": {},
+    },
+
+    "&-pageLabel": {
+      "@apply cursor-pointer m-sm": {},
+      width: "1.5em",
+      height: "1.75em",
+
+      "&[aria-disabled='true']": {
+        "@apply border-primary border-b cursor-default text-primary": {},
+      },
+    },
+
+    "&-prevNextButton": {
+      "@apply ml-4 inline-flex items-center sm:w-auto inline-flex flex-row leading-none":
+        {},
+
+      "&[data-reverse=true]": {
+        "@apply mr-4 ml-0 flex-row-reverse": {},
+
+        ".pagination-prevNextButton-label": {
+          "@apply mr-sm ml-0": {},
+        },
+      },
+
+      "&[disabled]": {
+        "@apply opacity-50 cursor-default": {},
+      },
+
+      "&[disabled='false']": {
+        "&-label, &-icon": {
+          "@apply cursor-pointer": {},
+        },
+      },
+
+      "&-label": {
+        "@apply hidden sm:inline ml-sm leading-none": {},
+      },
+
+      "&-icon": {
+        "@apply m-1 align-text-bottom leading-none flex items-center": {},
+
+        ".material-icons-outlined": {
+          fontSize: ".675em",
+        },
+      },
+    },
+
+    "&-ellipsis": {
+      "@apply inline-block w-16 text-center": {},
+    },
+  },
+});
---
+++ packages/pagination/src/pagination.tsx
@@ -0,0 +1,172 @@
+import { colors, DefaultProps } from "@sk-web-gui/theme";
+import { cx, __DEV__ } from "@sk-web-gui/utils";
+import React, { useEffect, useState } from "react";
+
+import { usePaginationClass } from "./styles";
+
+export interface IPaginationProps extends DefaultProps {
+  /* Total amount of pages */
+  pages: number;
+  /* Currently active page */
+  activePage: number;
+  /* Handles page number change */
+  changePage: (page: number) => void;
+  /* Size of the button */
+  size?: "sm" | "md" | "lg";
+}
+
+export interface PaginationProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    IPaginationProps {}
+
+export const Pagination = React.forwardRef<HTMLDivElement, PaginationProps>(
+  (props, ref) => {
+    const {
+      pages = 1,
+      activePage = 1,
+      changePage,
+      children,
+      size = "md",
+      ...rest
+    } = props;
+
+    const classes = usePaginationClass({
+      size,
+    });
+
+    const minPage = 1;
+    const pagesBeforeAndAfter = 2;
+    const [currentPage, setCurrentPage] = useState(1);
+    const internalHandleChange = (page: number) => {
+      if (page > minPage - 1 && page < pages + 1) {
+        setCurrentPage(page);
+      }
+    };
+
+    const handleClick = (pageNumber: number) => {
+      changePage(pageNumber);
+      internalHandleChange(pageNumber);
+    };
+
+    const shouldShowLabel: (idx: number, cP: number, pBaA: number) => boolean =
+      (idx) =>
+        idx !== 0 &&
+        idx !== pages - 1 &&
+        idx > currentPage - pagesBeforeAndAfter - 2 &&
+        idx < currentPage + pagesBeforeAndAfter;
+
+    useEffect(() => {
+      internalHandleChange(activePage);
+    }, [activePage, internalHandleChange]);
+
+    const pageLabel = (pageNumber: number) => {
+      const isDisabled = currentPage === pageNumber;
+      return (
+        <button
+          type="button"
+          tabIndex={0}
+          aria-current={currentPage === pageNumber}
+          aria-disabled={isDisabled}
+          aria-label={
+            currentPage === pageNumber
+              ? `sida ${pageNumber}, Nuvarande sida`
+              : `Gå till sida ${pageNumber}`
+          }
+          className={cx(`pagination-pageLabel`)}
+          onClick={() => handleClick(pageNumber)}
+          key={`page${pageNumber}`}
+        >
+          {pageNumber}
+        </button>
+      );
+    };
+
+    const prevNextButton: (props: {
+      label: string;
+      icon: JSX.Element;
+      triggerNumber: number;
+      step: number;
+      reverse?: boolean;
+    }) => JSX.Element = ({
+      label,
+      icon,
+      triggerNumber = 1,
+      step = 1,
+      reverse = false,
+    }) => {
+      const isDisabled = currentPage === triggerNumber;
+      return (
+        <button
+          type="button"
+          tabIndex={0}
+          aria-label={`${label} sida`}
+          aria-disabled={isDisabled}
+          disabled={isDisabled}
+          data-reverse={reverse}
+          onClick={() => isDisabled || handleClick(currentPage + step)}
+          className={cx(`pagination-prevNextButton`)}
+        >
+          <span className={cx(`pagination-prevNextButton-label`)}>{label}</span>
+          <span className={cx(`pagination-prevNextButton-icon`)}>{icon}</span>
+        </button>
+      );
+    };
+
+    const ellipsis = (
+      <span className="pagination-prevNextButton-ellipsis">...</span>
+    );
+
+    return (
+      <nav className={cx(classes)} {...rest} aria-label="pagination">
+        <ul className="pagination-list">
+          <li className="inline">
+            {prevNextButton({
+              label: "Föregående",
+              icon: (
+                <span className="material-icons-outlined" aria-hidden="true">
+                  keyboard_double_arrow_left
+                </span>
+              ),
+              triggerNumber: minPage,
+              step: -1,
+              reverse: true,
+            })}
+          </li>
+          <li className="inline">{pageLabel(1)}</li>
+          {currentPage > pagesBeforeAndAfter + 2 && (
+            <li className="inline">{ellipsis}</li>
+          )}
+          {[...Array(pages)].map((_, idx: number) => {
+            return (
+              shouldShowLabel(idx, currentPage, pagesBeforeAndAfter) && (
+                <li key={`pageLabel-${idx}`} className="inline">
+                  {pageLabel(idx + 1)}
+                </li>
+              )
+            );
+          })}
+          {currentPage < pages - pagesBeforeAndAfter - 1 && (
+            <li className="inline">{ellipsis}</li>
+          )}
+          <li className="inline">{pageLabel(pages)}</li>
+          <li className="inline">
+            {prevNextButton({
+              label: "Nästa",
+              icon: (
+                <span className="material-icons-outlined" aria-hidden="true">
+                  keyboard_double_arrow_right
+                </span>
+              ),
+              triggerNumber: pages,
+              step: 1,
+            })}
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+);
+
+if (__DEV__) {
+  Pagination.displayName = "Pagination";
+}
