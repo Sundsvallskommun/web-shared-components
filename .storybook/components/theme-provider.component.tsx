c13daa5495a18fd01940496c59bb1a34652de7c8
Andréas Idh <andreas.idh@devize.se>
2022-05-31 11:02:29 +0200

    Added GUI provider to docpages

---
+++ .storybook/components/theme-provider.component.tsx
@@ -0,0 +1,8 @@
+import { GuiProvider } from "./../../packages/theme";
+
+export function ThemeProvider({ children }) {
+
+    return <GuiProvider /*theme={theme}*/ colorScheme="light">
+        {children}
+    </GuiProvider>;
+}
