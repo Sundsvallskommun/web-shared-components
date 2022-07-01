1749041bac2a93a6a1a499276edf9e2dd2be4bd8
Tim Lind <tim.lind@sundsvall.se>
2022-07-01 10:50:48 +0200

    structure change

--- packages/theme/src/default-theme.ts
+++ packages/theme/src/default-theme.ts
@@ -8,7 +8,7 @@ export const lightScheme: ColorScheme = {
   colors: {
     bg: {
       base: "#ffffff",
-      fill: colors.background.one,
+      fill: colors.background.two,
     },
     text: {
       foreground: "#000000",
@@ -17,21 +17,17 @@ export const lightScheme: ColorScheme = {
     primary: colors.vattjom,
     secondary: colors.gronsta,
     neutral: colors.svartvik,
-    success: colors.gronsta,
-    info: colors.vattjom,
-    warning: colors.warning,
-    error: colors.error,
   },
 };
 
 // dark theme
 export const darkScheme: ColorScheme = {
-  id: "light",
+  id: "dark",
   type: "dark",
   colors: {
     bg: {
-      base: colors.gray[2],
-      fill: colors.gray[1],
+      base: "#ffffff",
+      fill: colors.background.two,
     },
     text: {
       foreground: colors.svartvik["100"],
@@ -40,10 +36,6 @@ export const darkScheme: ColorScheme = {
     primary: colors.vattjom,
     secondary: colors.gronsta,
     neutral: colors.svartvik,
-    success: colors.gronsta,
-    info: colors.vattjom,
-    warning: colors.warning,
-    error: colors.error,
   },
 };
 
