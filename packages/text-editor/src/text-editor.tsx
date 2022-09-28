bc14ce62a1197a29940e85679e9874324654de1e
Henrik Sandström <henrik.sandstrom@sundsvall.se>
2022-09-28 07:39:13 +0200

    Export dummy component so as not to destroy for everyone who bumps version..

--- packages/text-editor/src/text-editor.tsx
+++ packages/text-editor/src/text-editor.tsx
@@ -1,46 +1,50 @@
-import { __DEV__ } from '@sk-web-gui/utils';
-import { DeltaStatic, Sources } from 'quill';
-import ReactQuill, { Value, UnprivilegedEditor } from 'react-quill';
+// import { __DEV__ } from '@sk-web-gui/utils';
+import * as React from 'react';
+// import { DeltaStatic, Sources } from 'quill';
+// import ReactQuill, { Value, UnprivilegedEditor } from 'react-quill';
+// import 'react-quill/dist/quill.snow.css';
 
-type propTypes = {
-  theme: string;
-  value: Value;
-  onChange: ((value: string, delta: DeltaStatic, source: Sources, editor: UnprivilegedEditor) => void) | undefined;
-  advanced: boolean;
-};
+// type propTypes = {
+//   theme: string;
+//   value: Value;
+//   onChange: ((value: string, delta: DeltaStatic, source: Sources, editor: UnprivilegedEditor) => void) | undefined;
+//   advanced: boolean;
+// };
 
-export const TextEditor = (props: propTypes) => {
-  const { theme, value, onChange, advanced = false } = props;
+// export const TextEditor = (props: propTypes) => {
+//   const { theme, value, onChange, advanced = false } = props;
 
-  const simpleOptions = [
-    [{ header: 1 }],
-    [{ header: 2 }],
-    ['bold'],
-    ['italic'],
-    [{ list: 'bullet' }],
-    [{ list: 'ordered' }],
-  ];
+//   const simpleOptions = [
+//     [{ header: 1 }],
+//     [{ header: 2 }],
+//     ['bold'],
+//     ['italic'],
+//     [{ list: 'bullet' }],
+//     [{ list: 'ordered' }],
+//   ];
 
-  const advancedOptions = [
-    [{ header: 1 }, { header: 2 }], // custom button values
-    ['bold', 'italic', 'underline', 'strike'],
-    ['blockquote', 'code-block'],
-    [{ list: 'bullet' }],
-    [{ list: 'ordered' }],
-    [{ script: 'sub' }, { script: 'super' }], // superscript/subscript
-    [{ indent: '-1' }, { indent: '+1' }], // outdent/indent
-    [{ direction: 'rtl' }], // text direction
-    [{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
-    [{ header: [1, 2, 3, 4, 5, 6, false] }],
-    [{ color: [] }, { background: [] }], // dropdown with defaults from theme
-    [{ font: [] }],
-    [{ align: [] }],
-    ['clean'], // remove formatting button
-  ];
+//   const advancedOptions = [
+//     [{ header: 1 }, { header: 2 }], // custom button values
+//     ['bold', 'italic', 'underline', 'strike'],
+//     ['blockquote', 'code-block'],
+//     [{ list: 'bullet' }],
+//     [{ list: 'ordered' }],
+//     [{ script: 'sub' }, { script: 'super' }], // superscript/subscript
+//     [{ indent: '-1' }, { indent: '+1' }], // outdent/indent
+//     [{ direction: 'rtl' }], // text direction
+//     [{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
+//     [{ header: [1, 2, 3, 4, 5, 6, false] }],
+//     [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+//     [{ font: [] }],
+//     [{ align: [] }],
+//     ['clean'], // remove formatting button
+//   ];
 
-  const modules = {
-    toolbar: advanced ? advancedOptions : simpleOptions,
-  };
+//   const modules = {
+//     toolbar: advanced ? advancedOptions : simpleOptions,
+//   };
 
-  return <ReactQuill theme={theme} value={value} onChange={onChange} modules={modules} />;
-};
+//   return <ReactQuill theme={theme} value={value} onChange={onChange} modules={modules} />;
+// };
+
+export const TextEditor = () => <textarea></textarea>;
