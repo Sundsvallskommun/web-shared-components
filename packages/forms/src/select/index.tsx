d82a52465bd6b234e8812063ce330b025dfb350e
Tim Lind <tim.lind@sundsvall.se>
2022-10-24 13:25:08 +0200

    Select: Fix for window undefined

--- packages/forms/src/select/index.tsx
+++ packages/forms/src/select/index.tsx
@@ -27,9 +27,10 @@ export const Select = React.forwardRef<HTMLSelectElement, SelectProps>((props, r
     }
   };
 
-  window.onresize = setListBoundingBox;
-
   useEffect(() => {
+    if (window) {
+      window.onresize = setListBoundingBox;
+    }
     setListBoundingBox();
   }, []);
 
