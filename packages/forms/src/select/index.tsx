fcc98be7a1cc0d9584c91cd4055b542422173d38
Tim Lind <tim.lind@sundsvall.se>
2022-10-25 09:52:39 +0200

    Reverting Select to use absolute on option list

--- packages/forms/src/select/index.tsx
+++ packages/forms/src/select/index.tsx
@@ -12,31 +12,8 @@ export type SelectProps = Omit<InputProps, 'onChange'> & { onChange: (value: str
 export const Select = React.forwardRef<HTMLSelectElement, SelectProps>((props, ref) => {
   const { className, onChange, value, placeholder = '', children, disabled, size = 'md', ...rest } = props;
   const [selectedValue, setSelectedValue] = useState(value ? value : '');
-  const visualInputRef = useRef<HTMLButtonElement>(null);
+
   const classes = useSelectClass({ size, disabled });
-  const [width, setWidth] = useState(0);
-  const [listPos, setListPos] = useState({ top: 0, left: 0 });
-
-  const setListBoundingBox = () => {
-    if (visualInputRef && visualInputRef.current) {
-      setWidth(visualInputRef.current.getBoundingClientRect().width);
-      setListPos({
-        top: visualInputRef.current.getBoundingClientRect().top + visualInputRef.current.getBoundingClientRect().height,
-        left: visualInputRef.current.getBoundingClientRect().left,
-      });
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener('resize', setListBoundingBox);
-    window.addEventListener('scroll', setListBoundingBox);
-    setListBoundingBox();
-
-    return () => {
-      window.removeEventListener('resize', setListBoundingBox);
-      window.removeEventListener('scroll', setListBoundingBox);
-    };
-  }, []);
 
   const handleOnChange = (value: any) => {
     setSelectedValue(value);
@@ -54,9 +31,11 @@ export const Select = React.forwardRef<HTMLSelectElement, SelectProps>((props, r
             disabled={disabled ? disabled : undefined}
             aria-disabled={disabled ? disabled : undefined}
           />
-          <Listbox.Button ref={visualInputRef} className="w-full">
+          <Listbox.Button as={Fragment}>
             <Input
-              as="div"
+              ref={ref}
+              as="button"
+              type="button"
               size={size}
               disabled={disabled ? disabled : undefined}
               aria-disabled={disabled ? disabled : undefined}
@@ -68,11 +47,7 @@ export const Select = React.forwardRef<HTMLSelectElement, SelectProps>((props, r
             </Input>
           </Listbox.Button>
           {open && (
-            <Listbox.Options
-              style={{ width: width, top: listPos.top, left: listPos.left }}
-              static
-              className={cx('form-select-list')}
-            >
+            <Listbox.Options static className={cx('form-select-list')}>
               {children &&
                 (children as any).map((option: any, index: number) => (
                   <Listbox.Option key={`form-select-option-${index}`} value={option.props.children} as={Fragment}>
