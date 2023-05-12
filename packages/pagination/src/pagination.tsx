d6066aee7e16beb96bf03da324d5184230f0c5dc
Jerker Osatie <jerker.haggstrom.osatie@sundsvall.se>
2023-05-12 11:11:17 +0200

    Uppdaterat tillgänglighet på paginering

--- packages/pagination/src/pagination.tsx
+++ packages/pagination/src/pagination.tsx
@@ -1,6 +1,6 @@
 import { DefaultProps } from '@sk-web-gui/utils';
 import { cx, __DEV__ } from '@sk-web-gui/utils';
-import React, { useEffect, useState } from 'react';
+import React, { KeyboardEvent, useEffect, useState } from 'react';
 import KeyboardDoubleArrowLeftOutlinedIcon from '@mui/icons-material/KeyboardDoubleArrowLeftOutlined';
 import KeyboardDoubleArrowRightOutlinedIcon from '@mui/icons-material/KeyboardDoubleArrowRightOutlined';
 import { usePaginationClass } from './styles';
@@ -27,6 +27,8 @@ export interface IPaginationProps extends DefaultProps {
   nextLabel?: string;
   /* Label on previous button */
   prevLabel?: string;
+  /* Hides the text on prev and next buttons */
+  hidePrevNextLabel?: boolean;
   /* Stretch width to fit container */
   fitContainer?: boolean;
   /* Always show same number of pages */
@@ -49,6 +51,7 @@ export const Pagination = React.forwardRef<HTMLDivElement, PaginationProps>((pro
     pagesBefore = 2,
     pagesAfter = 2,
     className,
+    hidePrevNextLabel,
     nextLabel = 'Nästa',
     prevLabel = 'Föregående',
     fitContainer = false,
@@ -96,13 +99,15 @@ export const Pagination = React.forwardRef<HTMLDivElement, PaginationProps>((pro
   }, [activePage, internalHandleChange]);
 
   const pageLabel = (pageNumber: number) => {
-    const isDisabled = currentPage === pageNumber;
+    const active = currentPage === pageNumber;
     return (
       <button
+        tabIndex={-1}
+        onKeyDown={keyboardHandler}
         type="button"
-        aria-current={currentPage === pageNumber}
-        aria-disabled={isDisabled}
-        aria-label={currentPage === pageNumber ? `sida ${pageNumber}, Nuvarande sida` : `Gå till sida ${pageNumber}`}
+        role="menuitem"
+        aria-current={active}
+        aria-label={active ? `sida ${pageNumber}, Nuvarande sida` : `Gå till sida ${pageNumber}`}
         className={cx(`pagination-pageLabel`)}
         onClick={() => handleClick(pageNumber)}
         key={`page${pageNumber}`}
@@ -118,10 +123,15 @@ export const Pagination = React.forwardRef<HTMLDivElement, PaginationProps>((pro
     triggerNumber: number;
     step: number;
     reverse?: boolean;
-  }) => JSX.Element = ({ label, icon, triggerNumber = 1, step = 1, reverse = false }) => {
+    tabIndex?: number;
+    index: number;
+  }) => JSX.Element = ({ label, icon, triggerNumber = 1, step = 1, reverse = false, tabIndex, index }) => {
     const isDisabled = currentPage === triggerNumber;
     return (
       <button
+        onKeyDown={keyboardHandler}
+        tabIndex={tabIndex}
+        role="menuitem"
         type="button"
         aria-label={`${label} sida`}
         aria-disabled={isDisabled}
@@ -130,13 +140,97 @@ export const Pagination = React.forwardRef<HTMLDivElement, PaginationProps>((pro
         onClick={() => isDisabled || handleClick(currentPage + step)}
         className={cx(`pagination-prevNextButton`)}
       >
-        <span className={cx(`pagination-prevNextButton-label`)}>{label || ' '}</span>
+        <span className={cx(`pagination-prevNextButton-label`)}>{hidePrevNextLabel ? '' : label}</span>
         <span className={cx(`pagination-prevNextButton-icon`)}>{icon}</span>
       </button>
     );
   };
 
-  const ellipsis = <span className={cx('pagination-prevNextButton-ellipsis')}>…</span>;
+  const ellipsis = (
+    <span className={cx('pagination-prevNextButton-ellipsis')} aria-hidden={true} role="separator">
+      …
+    </span>
+  );
+
+  const goToNextButton = (button: HTMLElement, index: number, total: number) => {
+    if (index + 1 === total) {
+      goToFirstButton(button, total);
+    } else {
+      const next = button?.parentElement?.nextSibling?.firstChild as HTMLElement;
+      if (next && !next?.ariaHidden && next.ariaDisabled !== 'true') {
+        next.focus();
+      } else {
+        goToNextButton(next, index + 1, total);
+      }
+    }
+  };
+
+  const goToPreviousButton = (button: HTMLElement, index: number, total: number) => {
+    if (index === 0) {
+      goToLastButton(button, total);
+    } else {
+      const previous = button?.parentElement?.previousSibling?.firstChild as HTMLElement;
+      if (previous && !previous.ariaHidden && previous.ariaDisabled !== 'true') {
+        previous.focus();
+      } else {
+        goToPreviousButton(previous, index - 1, total);
+      }
+    }
+  };
+
+  const goToFirstButton = (button: HTMLElement, total: number) => {
+    const first = button?.parentElement?.parentElement?.firstChild?.firstChild as HTMLElement;
+    if (first && first.ariaDisabled !== 'true') {
+      first.focus();
+    } else {
+      goToNextButton(first, 0, total);
+    }
+  };
+
+  const goToLastButton = (button: HTMLElement, total: number) => {
+    const last = button?.parentElement?.parentElement?.lastChild?.firstChild as HTMLElement;
+    if (last && last.ariaDisabled !== 'true') {
+      last.focus();
+    } else {
+      goToPreviousButton(last, total - 1, total);
+    }
+  };
+
+  const keyboardHandler = (event: KeyboardEvent<HTMLButtonElement>) => {
+    const allItems = Array.from(event.currentTarget?.parentElement?.parentElement?.children as any);
+    const total = allItems?.length;
+    const index = allItems.indexOf(event.currentTarget?.parentElement);
+
+    if (event.key === 'ArrowRight' || event.key === 'ArrowDown') {
+      event.preventDefault();
+      goToNextButton(event.currentTarget, index, total);
+    }
+    if (event.key === 'ArrowLeft' || event.key === 'ArrowUp') {
+      event.preventDefault();
+      goToPreviousButton(event.currentTarget, index, total);
+    }
+    if (event.key === 'Home') {
+      event.preventDefault();
+      goToFirstButton(event.currentTarget, total);
+    }
+
+    if (event.key === 'End') {
+      event.preventDefault();
+      goToLastButton(event.currentTarget, total);
+    }
+
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      if (index + 1 === total && activePage === pages - 1) {
+        goToFirstButton(event.currentTarget, total);
+      }
+
+      if (index === 0 && activePage === minPage + 1) {
+        goToLastButton(event.currentTarget, total);
+      }
+      event.currentTarget.click();
+    }
+  };
 
   return (
     <nav className={cx(classes, className)} {...rest} aria-label="pagination">
@@ -154,41 +248,54 @@ export const Pagination = React.forwardRef<HTMLDivElement, PaginationProps>((pro
           ))}
         </Select>
       ) : (
-        <ul className="pagination-list">
-          <li className="pagination-list-item prev-next">
+        <ul className="pagination-list" role="menubar" aria-orientation="horizontal">
+          <li className="pagination-list-item prev-next" role="none">
             {prevNextButton({
               label: prevLabel,
               icon: <KeyboardDoubleArrowLeftOutlinedIcon aria-hidden="true" />,
               triggerNumber: minPage,
               step: -1,
               reverse: true,
+              tabIndex: activePage == pages ? undefined : -1,
+              index: 0,
             })}
           </li>
-          {(activePage === 1 || showFirst || pagesBefore >= currentPage - 1) && (
-            <li className="pagination-list-item">{pageLabel(1)}</li>
+          {(activePage === 1 || showFirst || pagesBefore >= currentPage - 1 || pages <= shownBefore + shownAfter) && (
+            <li className="pagination-list-item" role="none">
+              {pageLabel(1)}
+            </li>
           )}
           {currentPage > pagesBefore + 2 && showFirst && <li className="pagination-list-item ellipsis">{ellipsis}</li>}
           {[...Array(pages)].map((_, idx: number) => {
             return (
               shouldShowLabel(idx, currentPage, pagesBefore) && (
-                <li key={`pageLabel-${idx}`} className="pagination-list-item">
+                <li key={`pageLabel-${idx}`} className="pagination-list-item" role="none">
                   {pageLabel(idx + 1)}
                 </li>
               )
             );
           })}
           {currentPage < pages - pagesAfter - 1 && showLast && (
-            <li className="pagination-list-item ellipsis">{ellipsis}</li>
+            <li className="pagination-list-item ellipsis" role="none">
+              {ellipsis}
+            </li>
           )}
-          {(showLast || activePage === pages || pagesAfter > pages - (currentPage + 1)) && (
-            <li className="pagination-list-item">{pageLabel(pages)}</li>
+          {(showLast ||
+            activePage === pages ||
+            pagesAfter > pages - (currentPage + 1) ||
+            pages <= shownBefore + shownAfter) && (
+            <li className="pagination-list-item" role="none">
+              {pageLabel(pages)}
+            </li>
           )}
-          <li className="pagination-list-item  prev-next">
+          <li className="pagination-list-item  prev-next" role="none">
             {prevNextButton({
               label: nextLabel,
               icon: <KeyboardDoubleArrowRightOutlinedIcon aria-hidden="true" />,
               triggerNumber: pages,
               step: 1,
+              tabIndex: activePage == pages ? -1 : undefined,
+              index: pages + 1,
             })}
           </li>
         </ul>
