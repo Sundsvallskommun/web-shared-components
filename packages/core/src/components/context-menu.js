b613946e83de10c8a190d799c7568c05006a5c36
Tim Lind <tim.lind@sundsvall.se>
2023-10-24 08:20:22 +0200

    changed to minimum-rounding, same as usermenu

--- packages/core/src/components/context-menu.js
+++ packages/core/src/components/context-menu.js
@@ -7,7 +7,7 @@ module.exports = ContextMenu = () => ({
     },
 
     '.context-menu-items': {
-      '@apply absolute rounded-md bg-white border border-body z-10 drop-shadow-md mt-sm flex flex-col justify-start items-start':
+      '@apply absolute rounded bg-white border border-body z-10 drop-shadow-md mt-sm flex flex-col justify-start items-start':
         {},
       '.context-menu-item': {
         '@apply text-base text-body hover:text-white focus-within:text-white min-h-[3.5rem] w-full flex justify-start items-center':
