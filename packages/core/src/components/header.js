e1b708c7dab6d0c7d25159bbded7e9600d41c24e
Jerker Osatie <jerker.haggstrom.osatie@sundsvall.se>
2023-11-06 13:41:37 +0100

    fix: header breakpoints

--- packages/core/src/components/header.js
+++ packages/core/src/components/header.js
@@ -1,24 +1,24 @@
 module.exports = Header = () => ({
   '.sk-header': {
-    '@apply bg-background-content p-12 md:px-32 md:pt-24 md:pb-20 lg:pt-32 lg:pb-20 lg:px-80 lg:h-auto lg:min-h-0': {},
+    '@apply bg-background-content p-12 md:px-32 md:pt-24 md:pb-20 xl:pt-32 xl:pb-20 xl:px-80 xl:h-auto xl:min-h-0': {},
     '@apply shadow-100': {},
 
     '&-container': {
-      '@apply flex flex-col gap-20 lg:gap-24': {},
+      '@apply flex flex-col gap-20 xl:gap-24': {},
     },
 
     '&-top-content': {
       '@apply flex items-center w-full justify-between m-auto flex-shrink-0 py-4 gap-20': {},
     },
     '&-bottom-content': {
-      '@apply hidden md:flex items-center w-full justify-start p-8 lg:p-0': {},
+      '@apply hidden md:flex items-center w-full justify-start p-8 xl:p-0': {},
     },
 
     '&-usermenu': {
-      '@apply hidden lg:flex-shrink md:flex lg:items-center lg:w-auto': {},
+      '@apply hidden xl:flex-shrink md:flex xl:items-center xl:w-auto': {},
 
       '&-content': {
-        '@apply relative flex flex-grow justify-end items-center lg:w-auto': {},
+        '@apply relative flex flex-grow justify-end items-center xl:w-auto': {},
       },
     },
     '&-mobilemenu': {
