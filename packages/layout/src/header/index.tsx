f2ea86398c21cae2000d10abf08842822a646edf
Jerker Osatie <jerker.haggstrom.osatie@sundsvall.se>
2023-12-15 12:59:58 +0100

    fix: updated header with new logo component

--- packages/layout/src/header/index.tsx
+++ packages/layout/src/header/index.tsx
@@ -1,15 +1,12 @@
 import { HeaderComponent, HeaderComponentProps } from './header';
-import { HeaderLogoText } from './header-logotext';
 
 interface HeaderProps extends React.ForwardRefExoticComponent<HeaderComponentProps> {
   Component: typeof HeaderComponent;
-  LogoText: typeof HeaderLogoText;
 }
 
 export const Header = {
   ...HeaderComponent,
   Component: HeaderComponent,
-  LogoText: HeaderLogoText,
 } as HeaderProps;
 
 export type { HeaderProps };
