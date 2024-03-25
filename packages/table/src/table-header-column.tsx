5ed9b0c9efda373ff7bc6b6804e22e8aca97a4f6
Jerker Osatie <jerker.haggstrom.osatie@sundsvall.se>
2024-03-25 14:09:19 +0100

    feat: header can now be sticky

--- packages/table/src/table-header-column.tsx
+++ packages/table/src/table-header-column.tsx
@@ -1,5 +1,4 @@
 import { cx } from '@sk-web-gui/utils';
-import { useElementSize } from 'usehooks-ts';
 
 import React from 'react';
 
@@ -9,18 +8,10 @@ interface TableHeaderColumnProps extends React.ComponentPropsWithoutRef<'th'> {
 
 export const TableHeaderColumn = React.forwardRef<HTMLTableCellElement, TableHeaderColumnProps>((props, ref) => {
   const { className, children, sticky, ...rest } = props;
-  const [innerRef, { width }] = useElementSize();
 
   return (
     <th ref={ref} className={'sk-table-th'} data-sticky={sticky} {...rest}>
-      {sticky && (
-        <span className="sk-table-sticky-col" style={{ width: `${width}px` }}>
-          <span className={cx('sk-table-col-content', className)}>{children}</span>
-        </span>
-      )}
-      <span ref={innerRef} className={cx('sk-table-col-content', className)} aria-hidden={sticky}>
-        {children}
-      </span>
+      <span className={cx('sk-table-col-content', className)}>{children}</span>
     </th>
   );
 });
