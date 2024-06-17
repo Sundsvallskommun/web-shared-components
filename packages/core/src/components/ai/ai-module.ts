73896aef076a7265542e3ffa49ac0f4b719a1f39
Jerker Osatie <jerker.haggstrom.osatie@sundsvall.se>
2024-06-17 10:53:57 +0200

    fix(ai): responsive module

--- packages/core/src/components/ai/ai-module.ts
+++ packages/core/src/components/ai/ai-module.ts
@@ -1,6 +1,7 @@
 export const AIModule = (colors: string[]) => ({
   '.sk-ai-module': {
     '@apply fixed': {},
+    '@apply max-w-full': {},
     '@apply z-50': {},
     '@apply overflow-hidden': {},
     '@apply flex flex-col gap-0 items-center justify-start': {},
@@ -11,6 +12,7 @@ export const AIModule = (colors: string[]) => ({
 
     '&-content': {
       '@apply flex w-full gap-0 h-full justify-between': {},
+      '@apply relative': {},
       '@apply overflow-hidden': {},
       '&-row': {
         '@apply flex flex-col': {},
@@ -23,7 +25,7 @@ export const AIModule = (colors: string[]) => ({
 
     '&-sidebar': {
       '@apply flex flex-col': {},
-      '@apply min-w-[22em]': {},
+      '@apply lg:w-[22em]': {},
       '@apply h-full': {},
       '@apply overflow-hidden': {},
       '@apply border-r-1 border-r-divider': {},
@@ -55,12 +57,12 @@ export const AIModule = (colors: string[]) => ({
       '@apply shrink-0': {},
     },
     '&[data-fullscreen="false"]': {
-      '@apply rounded-tl-groups': {},
-      '@apply max-w-[26em]': {},
-      '@apply w-[26em]': {},
-      '@apply max-h-[44em]': {},
+      '@apply md:rounded-tl-groups': {},
+      '@apply md:max-w-[26em]': {},
+      '@apply w-full md:w-[26em]': {},
+      '@apply md:max-h-[44em]': {},
       '&[data-docked="false"]': {
-        '@apply h-[44em]': {},
+        '@apply h-full md:h-[44em]': {},
       },
     },
 
@@ -87,7 +89,7 @@ export const AIModule = (colors: string[]) => ({
       },
     },
     '&[data-docked="true"]': {
-      '@apply h-64 w-[19em]': {},
+      '@apply h-64 w-full md:w-[19em]': {},
     },
     '&-header': {
       '@apply flex items-center justify-between': {},
@@ -133,7 +135,7 @@ export const AIModule = (colors: string[]) => ({
         '@apply pt-0 px-24': {},
         '@apply bg-transparent': {},
         '.sk-avatar': {
-          '@apply w-56 h-56': {},
+          '@apply w-40 h-40 lg:w-56 lg:h-56': {},
         },
         '.sk-ai-module-header-heading-name': {
           '@apply text-label-large text-dark-primary': {},
@@ -161,15 +163,58 @@ export const AIModule = (colors: string[]) => ({
     },
     '&-sessions': {
       '@apply px-20': {},
+      '&-group': {
+        '@apply py-8 px-12': {},
+      },
       '&-history': {
         '@apply flex flex-col p-8 gap-16': {},
         '&-list': {
           '@apply flex flex-col p-0 gap-8': {},
+          '[role="menuitem"]': {
+            '@apply max-w-full': {},
+            '@apply max-h-none h-auto text-wrap text-left': {},
+          },
         },
       },
     },
     '.sk-avatar': {
       '@apply transition-all duration-500': {},
+    },
+    '&-mobile-menu': {
+      '@apply rounded-0 py-16 absolute top-0 bottom-0 overflow-x-hidden': {},
+      '@apply px-0': {},
+      '@apply rounded-l-button': {},
+      '@apply bg-background-100': {},
+      '@apply shadow-100': {},
+      '@apply transition-all': {},
+
+      '&-top-bar': {
+        '@apply flex justify-between items-start': {},
+        '@apply pl-0 pr-16': {},
+        '.sk-ai-module-header': {
+          '@apply shrink': {},
+        },
+      },
+      '&-content': {
+        '@apply flex flex-col pt-8': {},
+        '[role="menuitem"]': {
+          '@apply w-full': {},
+        },
+      },
+      '&[data-show="true"]': {
+        '@apply left-16 right-0': {},
+      },
+      '&[data-show="false"]': {
+        '@apply w-0 h-0 left-full': {},
+        '.sk-ai-module-mobile-menu': {
+          '&-top-bar': {
+            '@apply hidden': {},
+          },
+          '&-content': {
+            '@apply hidden': {},
+          },
+        },
+      },
     },
   },
 });
