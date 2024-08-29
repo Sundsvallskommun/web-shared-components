1a60afa3deea5184e93404e53164fdc9047ca79d
Häggström Osatie Jerker <jerker.haggstrom.osatie@sundsvall.se>
2024-08-29 10:50:54 +0200

    ai service module wrapper

---
+++ packages/ai/src/components/ai-service-module/ai-service-module-wrapper.tsx
@@ -0,0 +1,10 @@
+import { cx } from '@sk-web-gui/utils/dist/types';
+import React from 'react';
+
+interface AIServiceModuleWrapperProps extends React.ComponentPropsWithoutRef<'div'> {}
+
+export const AIServiceModuleWrapper = React.forwardRef<HTMLDivElement, AIServiceModuleWrapperProps>((props, ref) => {
+  const { className, ...rest } = props;
+
+  return <div ref={ref} className={cx('sk-ai-service-module', className)} {...rest} />;
+});
