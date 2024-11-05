ad561ceac865988f83a95569c4fd2704a698375b
Tim Lind <tim.lind@sundsvall.se>
2024-11-05 10:24:36 +0100

    fix: ecosystem to use npx and explicit http-server, not yarn script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,9 +4,8 @@ module.exports = {
   apps: [
     {
       name: process.env.CONTAINER_NAME,
-      script: 'storybook:prod', // Directly specify the yarn script
-      interpreter: 'yarn', // Use yarn as the interpreter
-      args: ['-p', process.env.PORT], // Pass the port as an argument
+      script: 'npx',
+      args: ['http-server', 'storybook-static/', '-p', process.env.PORT],
       cwd: '.',
       exec_mode: 'fork',
       autorestart: true,
