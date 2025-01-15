3f7fdfda9bcd3c99f2bc469f0ce82181209a0df8
Tim Lind <tim.lind@sundsvall.se>
2025-01-15 16:20:42 +0100

    fix(eslint-errors): lucide-icon, and replaced to DynamicIcon

--- packages/lucide-icon/src/lucide-icon.tsx
+++ packages/lucide-icon/src/lucide-icon.tsx
@@ -1,33 +1,20 @@
 import { __DEV__, cx } from '@sk-web-gui/utils';
-import { icons } from 'lucide-react';
-import dynamicIconImports from 'lucide-react/dynamicIconImports';
 import React from 'react';
 import { Icon, type IconProps } from '@sk-web-gui/icon';
-
-type IconNames = keyof typeof dynamicIconImports;
+import { DynamicIcon, IconName } from 'lucide-react/dynamic';
 
 export interface LucideIconProps extends React.ComponentProps<IconProps['Component']> {
-  name?: IconNames;
-}
-
-function toPascalCase(text: string) {
-  return text.replace(/(^\w|-\w)/g, clearAndUpper);
-}
-
-function clearAndUpper(text: string) {
-  return text.replace(/-/, '').toUpperCase();
+  name?: IconName;
 }
 
 export const LucideIcon = React.forwardRef<HTMLSpanElement, LucideIconProps>((props, ref) => {
   const { name, className, ...rest } = props;
 
-  const LucideIcon = name ? icons[toPascalCase(name) as keyof typeof icons] : undefined;
-
   return (
     <Icon
       ref={ref}
       className={cx('sk-lucide-icon', className)}
-      icon={LucideIcon ? <LucideIcon /> : undefined}
+      icon={name ? <DynamicIcon name={name} /> : undefined}
       data-testid={name ? `sk-icon-${name}` : undefined}
       {...rest}
     />
