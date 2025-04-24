078398c3c0c70d6061377a087e003baac5dc7683
Jerker Osatie <jerker.osatie@devize.se>
2025-04-24 10:07:03 +0200

    finetuned spinner animation

--- packages/spinner/src/spinner.tsx
+++ packages/spinner/src/spinner.tsx
@@ -26,22 +26,22 @@ export const Spinner = React.forwardRef<HTMLDivElement, SpinnerProps>((props, re
     >
       <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <g>
-          <circle cx="12" cy="12" r="9.5" fill="none" strokeWidth="3" strokeLinecap="round">
+          <circle cx="12" cy="12" r="9.5" fill="none" strokeWidth="3.2" strokeLinecap="round">
             <animate
               attributeName="stroke-dasharray"
-              dur="1.5s"
+              dur="1.6s"
               calcMode="spline"
-              values="0 150;42 150;42 150;42 150"
-              keyTimes="0;0.475;0.95;1"
+              values="0 150;42 150;55 150;35 150;"
+              keyTimes="0;0.575;0.95;1"
               keySplines="0.42,0,0.58,1;0.42,0,0.58,1;0.42,0,0.58,1"
               repeatCount="indefinite"
             />
             <animate
               attributeName="stroke-dashoffset"
-              dur="1.5s"
+              dur="1.6s"
               calcMode="spline"
-              values="0;-16;-59;-59"
-              keyTimes="0;0.475;0.95;1"
+              values="0;-10;-54;-59;"
+              keyTimes="0;0.400;0.88;1"
               keySplines="0.42,0,0.58,1;0.42,0,0.58,1;0.42,0,0.58,1"
               repeatCount="indefinite"
             />
@@ -49,8 +49,8 @@ export const Spinner = React.forwardRef<HTMLDivElement, SpinnerProps>((props, re
           <animateTransform
             attributeName="transform"
             type="rotate"
-            dur="1.5s"
-            values="0 12 12;360 12 12"
+            dur="1.6s"
+            values="-90 12 12;270 12 12"
             repeatCount="indefinite"
           />
         </g>
