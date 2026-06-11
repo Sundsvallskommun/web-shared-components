9f6518305ce87ea3b4280d5ad64b8c81f28cc977
Jerker Osatie <jerker.haggstrom.osatie@sundsvall.se>
2026-06-11 11:59:08 +0200

    fix(ai): allow children in typing bubble

--- packages/core/src/components/ai/typing-bubble.ts
+++ packages/core/src/components/ai/typing-bubble.ts
@@ -1,7 +1,8 @@
 export const TypingBubble = () => ({
   '.sk-ai-typing-bubble': {
     '@apply inline-flex items-center justify-center': {},
-    '@apply p-12 gap-8 w-min': {},
+    '@apply text-small': {},
+    '@apply min-h-30 px-12 py-6 gap-6 w-min': {},
     '@apply bg-tertiary-surface': {},
     '@apply text-dark-secondary': {},
     '@apply rounded-button': {},
@@ -10,9 +11,11 @@ export const TypingBubble = () => ({
     },
     '&[data-position="right"]': {
       '@apply rounded-bl-0': {},
+      '@apply justify-start': {},
     },
     '&[data-inverted="true"]': {
       '@apply bg-inverted-tertiary-surface': {},
+      '@apply justify-end': {},
     },
   },
 });
